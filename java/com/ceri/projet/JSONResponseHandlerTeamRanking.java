8135973389718dab3894e4700833421a42774903
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-05-15 23:49:11 +0200

    activity item mvp OK

--- java/com/ceri/projet/JSONResponseHandlerTeamRanking.java
+++ java/com/ceri/projet/JSONResponseHandlerTeamRanking.java
@@ -1,89 +1,89 @@
-package com.ceri.projet;
-
-import android.util.JsonReader;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-/**
- * Process the response to a GET request to the Web service
- * https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=4414&s=1920
- * Responses must be provided in JSON.
- *
- */
-
-
-public class JSONResponseHandlerTeamRanking {
-
-    private static final String TAG = JSONResponseHandlerTeamRanking.class.getSimpleName();
-
-    private Item item;
-
-
-    public JSONResponseHandlerTeamRanking(Item item) {
-        this.item = item;
-    }
-
-    public Item getItem() { return this.item; }
-
-    /**
-     * @param response done by the Web service
-     *
-     */
-    public void readJsonStream(InputStream response) throws IOException {
-        JsonReader reader = new JsonReader(new InputStreamReader(response, "UTF-8"));
-        try {
-            readTable(reader);
-        } finally {
-            reader.close();
-        }
-    }
-
-    public void readTable(JsonReader reader) throws IOException {
-        reader.beginObject();
-        while (reader.hasNext()) {
-            String name = reader.nextName();
-            if (name.equals("table")) {
-                readArrayTable(reader);
-            } else {
-                reader.skipValue();
-            }
-        }
-        reader.endObject();
-    }
-
-
-    private void readArrayTable(JsonReader reader) throws IOException {
-        boolean foundTeam = false;
-        boolean gotTeamRanking = false;
-        reader.beginArray();
-        int position = 1; // ranking position
-        while (reader.hasNext() ) {
-            reader.beginObject();
-            while (reader.hasNext()) {
-                String name = reader.nextName();
-                if (!foundTeam) {
-                    if (name.equals("name")) {
-                        String s = reader.nextString();
-                        if(s.equals(this.item.getName())) {
-                            this.item.setRanking(position);
-                            gotTeamRanking = true;
-                        }
-                    } else if (name.equals("total") && gotTeamRanking){
-                        this.item.setTotalPoints(reader.nextInt());
-                        foundTeam = true;
-                    } else {
-                        reader.skipValue();
-                    }
-                }  else {
-                    reader.skipValue();
-                }
-            }
-            reader.endObject();
-            position++;
-        }
-        reader.endArray();
-    }
-
-}
+//package com.ceri.projet;
+//
+//import android.util.JsonReader;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//
+///**
+// * Process the response to a GET request to the Web service
+// * https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=4414&s=1920
+// * Responses must be provided in JSON.
+// *
+// */
+//
+//
+//public class JSONResponseHandlerTeamRanking {
+//
+//    private static final String TAG = JSONResponseHandlerTeamRanking.class.getSimpleName();
+//
+//    private Item item;
+//
+//
+//    public JSONResponseHandlerTeamRanking(Item item) {
+//        this.item = item;
+//    }
+//
+//    public Item getItem() { return this.item; }
+//
+//    /**
+//     * @param response done by the Web service
+//     *
+//     */
+//    public void readJsonStream(InputStream response) throws IOException {
+//        JsonReader reader = new JsonReader(new InputStreamReader(response, "UTF-8"));
+//        try {
+//            readTable(reader);
+//        } finally {
+//            reader.close();
+//        }
+//    }
+//
+//    public void readTable(JsonReader reader) throws IOException {
+//        reader.beginObject();
+//        while (reader.hasNext()) {
+//            String name = reader.nextName();
+//            if (name.equals("table")) {
+//                readArrayTable(reader);
+//            } else {
+//                reader.skipValue();
+//            }
+//        }
+//        reader.endObject();
+//    }
+//
+//
+//    private void readArrayTable(JsonReader reader) throws IOException {
+//        boolean foundTeam = false;
+//        boolean gotTeamRanking = false;
+//        reader.beginArray();
+//        int position = 1; // ranking position
+//        while (reader.hasNext() ) {
+//            reader.beginObject();
+//            while (reader.hasNext()) {
+//                String name = reader.nextName();
+//                if (!foundTeam) {
+//                    if (name.equals("name")) {
+//                        String s = reader.nextString();
+//                        if(s.equals(this.item.getName())) {
+//                            this.item.setRanking(position);
+//                            gotTeamRanking = true;
+//                        }
+//                    } else if (name.equals("total") && gotTeamRanking){
+//                        this.item.setTotalPoints(reader.nextInt());
+//                        foundTeam = true;
+//                    } else {
+//                        reader.skipValue();
+//                    }
+//                }  else {
+//                    reader.skipValue();
+//                }
+//            }
+//            reader.endObject();
+//            position++;
+//        }
+//        reader.endArray();
+//    }
+//
+//}
--- java/com/ceri/projet/RecyclerViewAdapter.java
+++ java/com/ceri/projet/RecyclerViewAdapter.java
@@ -1,95 +1,95 @@
-package com.ceri.projet;
-
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.io.File;
-import java.util.List;
-
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private static final String TAG = "RecyclerViewAdapter";
-
-    private Context context;
-    public List<Item> equipes;
-
-
-    public RecyclerViewAdapter(Context context, List<Item> equipes) {
-        this.context = context;
-        this.equipes = equipes;
-    }
-
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
-        ViewHolder holder = new ViewHolder(view);
-        return holder;
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-        Log.d(TAG, "onBindViewHolder: called");
-
-        final Item equipe = this.equipes.get(position);
-        final String nomEquipe = equipe.getName();
-        String dernierMatch = equipe.getLastEvent().toString();
-
-//        badge
-        String dirPath = this.context.getExternalFilesDir(null).toString();
-        File imageFile = new File(dirPath, equipe.getId() + ".png");
-        if(imageFile.exists()) {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inPreferredConfig = Bitmap.Config.RGB_565;
-            Bitmap img = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
-            ((ViewHolder) holder).image.setImageBitmap(img);
-        }
-
-
-        ((ViewHolder) holder).nomEquipe.setText(nomEquipe);
-        ((ViewHolder) holder).dernierMatch.setText(dernierMatch);
-        ((ViewHolder)holder).idBdd = equipe.getId();//
-        ((ViewHolder) holder).parentLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: clicked on : "+nomEquipe);
-
-                Intent intent = new Intent(v.getContext(), ItemActivity.class);
-                intent.putExtra(Item.TAG, equipe);
-                Log.d("wouloulou", "onClick: " + equipe.getId());
-                ((Activity)v.getContext()).startActivityForResult(intent, MainActivity.UPDATE_TEAM_REQUEST);
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        return this.equipes.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView image;
-        TextView nomEquipe;
-        TextView dernierMatch;
-        LinearLayout parentLayout;
-        long idBdd;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            image = itemView.findViewById(R.id.image);
-            nomEquipe = itemView.findViewById(R.id.nomEquipe);
-            dernierMatch = itemView.findViewById(R.id.dernierMatch);
-            parentLayout = itemView.findViewById(R.id.parentLayout);
-        }
-    }
-}
+//package com.ceri.projet;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
+//import android.support.v7.widget.RecyclerView;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.LinearLayout;
+//import android.widget.TextView;
+//
+//import java.io.File;
+//import java.util.List;
+//
+//public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+//    private static final String TAG = "RecyclerViewAdapter";
+//
+//    private Context context;
+//    public List<Item> equipes;
+//
+//
+//    public RecyclerViewAdapter(Context context, List<Item> equipes) {
+//        this.context = context;
+//        this.equipes = equipes;
+//    }
+//
+//
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
+//        ViewHolder holder = new ViewHolder(view);
+//        return holder;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+//        Log.d(TAG, "onBindViewHolder: called");
+//
+//        final Item equipe = this.equipes.get(position);
+//        final String nomEquipe = equipe.getName();
+//        String dernierMatch = equipe.getLastEvent().toString();
+//
+////        badge
+//        String dirPath = this.context.getExternalFilesDir(null).toString();
+//        File imageFile = new File(dirPath, equipe.getId() + ".png");
+//        if(imageFile.exists()) {
+//            BitmapFactory.Options options = new BitmapFactory.Options();
+//            options.inPreferredConfig = Bitmap.Config.RGB_565;
+//            Bitmap img = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+//            ((ViewHolder) holder).image.setImageBitmap(img);
+//        }
+//
+//
+//        ((ViewHolder) holder).nomEquipe.setText(nomEquipe);
+//        ((ViewHolder) holder).dernierMatch.setText(dernierMatch);
+//        ((ViewHolder)holder).idBdd = equipe.getId();//
+//        ((ViewHolder) holder).parentLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: clicked on : "+nomEquipe);
+//
+//                Intent intent = new Intent(v.getContext(), ItemActivity.class);
+//                intent.putExtra(Item.TAG, equipe);
+//                Log.d("wouloulou", "onClick: " + equipe.getId());
+//                ((Activity)v.getContext()).startActivityForResult(intent, MainActivity.UPDATE_TEAM_REQUEST);
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return this.equipes.size();
+//    }
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        ImageView image;
+//        TextView nomEquipe;
+//        TextView dernierMatch;
+//        LinearLayout parentLayout;
+//        long idBdd;
+//
+//        public ViewHolder(View itemView) {
+//            super(itemView);
+//            image = itemView.findViewById(R.id.image);
+//            nomEquipe = itemView.findViewById(R.id.nomEquipe);
+//            dernierMatch = itemView.findViewById(R.id.dernierMatch);
+//            parentLayout = itemView.findViewById(R.id.parentLayout);
+//        }
+//    }
+//}
