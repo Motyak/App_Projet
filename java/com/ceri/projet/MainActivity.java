b4a0a261db786e8a52ac82c9b307b6eb80972bf3
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-05-24 07:17:15 +0200

    ajout icone musee + changement titre

--- java/com/ceri/projet/MainActivity.java
+++ java/com/ceri/projet/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+//        toolbar.setTitle("Musée du CERI");
+        toolbar.setTitle("CERI");
         setSupportActionBar(toolbar);
         registerForContextMenu(findViewById(R.id.rvItems));
 
